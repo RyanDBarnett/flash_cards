7f05f417964f88b90d5b9439724c527063be6a66
RyanDBarnett <conner2468@gmail.com>
2020-03-05 11:55:19 -0700

    Complete round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,12 +1,10 @@
+require './lib/turn'
+
 class Round
+  attr_reader :deck
   def initialize(deck)
     @deck = deck
     @turns = []
-    @current_card = 'test'
-  end
-
-  def deck
-    @deck
   end
 
   def turns
@@ -14,6 +12,28 @@ class Round
   end
 
   def current_card
-    @current_card
+    @current_card = @deck.cards[@turns.length]
+  end
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+    new_turn
+  end
+
+  def number_correct
+    @turns.select{ |turn| turn.isCorrect? }.length
+  end
+
+  def number_correct_by_category(category)
+    @turns.select { |turn| turn.card.category == category }.length
+  end
+
+  def percent_correct
+    Float(number_correct) / deck.cards.length * 100
+  end
+
+  def percent_correct_by_category(category)
+    Float(number_correct_by_category(category)) / deck.cards.length * 100
   end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -4,44 +4,68 @@ require './lib/card'
 require './lib/deck'
 require './lib/round'
 require './lib/turn'
+require 'pry'
 
 class RoundTest < Minitest::Test
 
-  def test_it_exists
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card2 = Card.new("What is the capital of California?", "Sacramento", :Geography)
-    deck = Deck.new([card1, card2])
-    round = Round.new(deck)
+  def setup
+    @card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card2 = Card.new("What is the capital of California?", "Sacramento", :Geography)
+    @deck = Deck.new([@card1, @card2])
+    @round = Round.new(@deck)
+  end
 
-    assert_instance_of Round, round
+  def test_it_exists
+    assert_instance_of Round, @round
   end
 
   def test_it_has_a_deck
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card2 = Card.new("What is the capital of California?", "Sacramento", :Geography)
-    deck = Deck.new([card1, card2])
-    round = Round.new(deck)
-
-    assert_instance_of Deck, round.deck
+    assert_instance_of Deck, @round.deck
   end
 
   def test_it_has_a_current_card
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card2 = Card.new("What is the capital of California?", "Sacramento", :Geography)
-    deck = Deck.new([card1, card2])
-    round = Round.new(deck)
-
-    assert_equal Round.current_card, card1
+    assert_equal @round.current_card, @card1
   end
 
-  def test_it_can_take_turn
-    skip
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card2 = Card.new("What is the capital of California?", "Sacramento", :Geography)
-    deck = Deck.new([card1, card2])
-    round = Round.new(deck)
-    turn = round.take_turn('Juneau')
+  def test_it_can_take_turn 
+    turn = @round.take_turn('Juneau')
 
     assert_instance_of Turn, turn
+    assert_equal true, turn.isCorrect?
+    assert_equal 1, @round.turns.length
+  end
+
+  def test_the_current_card_has_changed
+    turn = @round.take_turn('Juneau')
+    
+    assert_equal @card2, @round.current_card
+  end
+
+  def test_it_can_return_the_number_of_correct_turns
+    turn1 = @round.take_turn('Juneau')
+
+    assert_equal 1, @round.number_correct
+
+    turn2 = @round.take_turn('Wrong')
+
+    assert_equal 1, @round.number_correct
+  end
+
+  def test_it_can_return_the_number_of_correct_answers_by_category
+    turn = @round.take_turn('Juneau')
+
+    assert_equal 1, @round.number_correct_by_category(:Geography)
+  end
+
+  def test_it_can_return_percentage_of_correct_answers
+    turn = @round.take_turn('Juneau')
+
+    assert_equal 50.0, @round.percent_correct
+  end
+
+  def test_it_can_return_percentage_of_correct_answers_by_category
+    turn = @round.take_turn('Juneau')
+    
+    assert_equal 50.0, @round.percent_correct_by_category(:Geography)
   end
 end
